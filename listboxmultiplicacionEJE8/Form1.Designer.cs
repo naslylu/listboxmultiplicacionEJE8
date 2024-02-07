4a24d0ffcd53e5a47547235824ad82e03f8e0418
naslylu <jaquezlu90@gmail.com>
2024-02-07 05:25:23 -0400

    Agregar archivos de proyecto.

---
+++ listboxmultiplicacionEJE8/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace listboxmultiplicacionEJE8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Numero = new System.Windows.Forms.TextBox();
+            this.lst_Tabla = new System.Windows.Forms.ListBox();
+            this.btn_Ingresar = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Salir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero";
+            // 
+            // txt_Numero
+            // 
+            this.txt_Numero.Location = new System.Drawing.Point(150, 42);
+            this.txt_Numero.Name = "txt_Numero";
+            this.txt_Numero.Size = new System.Drawing.Size(100, 20);
+            this.txt_Numero.TabIndex = 1;
+            // 
+            // lst_Tabla
+            // 
+            this.lst_Tabla.FormattingEnabled = true;
+            this.lst_Tabla.Location = new System.Drawing.Point(150, 97);
+            this.lst_Tabla.Name = "lst_Tabla";
+            this.lst_Tabla.Size = new System.Drawing.Size(120, 95);
+            this.lst_Tabla.TabIndex = 2;
+            // 
+            // btn_Ingresar
+            // 
+            this.btn_Ingresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Ingresar.Location = new System.Drawing.Point(369, 45);
+            this.btn_Ingresar.Name = "btn_Ingresar";
+            this.btn_Ingresar.Size = new System.Drawing.Size(112, 23);
+            this.btn_Ingresar.TabIndex = 3;
+            this.btn_Ingresar.Text = "Mostrar Tabla";
+            this.btn_Ingresar.UseVisualStyleBackColor = true;
+            this.btn_Ingresar.Click += new System.EventHandler(this.btn_Ingresar_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(386, 97);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_Limpiar.TabIndex = 4;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = true;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Salir
+            // 
+            this.btn_Salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Salir.Location = new System.Drawing.Point(386, 150);
+            this.btn_Salir.Name = "btn_Salir";
+            this.btn_Salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_Salir.TabIndex = 5;
+            this.btn_Salir.Text = "Salir";
+            this.btn_Salir.UseVisualStyleBackColor = true;
+            this.btn_Salir.Click += new System.EventHandler(this.btn_Salir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tabla de multiplicacion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Salir);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Ingresar);
+            this.Controls.Add(this.lst_Tabla);
+            this.Controls.Add(this.txt_Numero);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Numero;
+        private System.Windows.Forms.ListBox lst_Tabla;
+        private System.Windows.Forms.Button btn_Ingresar;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Salir;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ listboxmultiplicacionEJE8/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listboxmultiplicacionEJE8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_Numero.Clear();
+            lst_Tabla.Items.Clear();
+        }
+
+        private void btn_Ingresar_Click(object sender, EventArgs e)
+        {
+            lst_Tabla.Items.Clear();
+
+           
+            if (int.TryParse(txt_Numero.Text, out int numero))
+            {
+                
+                for (int i = 1; i <= 12; i++)
+                {
+                    int resultado = numero * i;
+                    lst_Tabla.Items.Add($"{numero} x {i} = {resultado}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un numero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_Salir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
